16c76c80e16f09c26111cbdf1715df5f87c31aee
xiadd <xiadd0102@gmail.com>
2019-03-01 15:55:53 +0800

    add husky

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { inject, observer } from 'mobx-react'
 import { Switch, Button } from 'antd'
 import { withTranslation } from 'react-i18next'
-const a = 111
 
 const HeaderContainer = styled.div`
   width: 100%;
