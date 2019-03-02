a601a74657355d9b839db8357411bfe9ab85a6d3
xiadd <xiadd0102@gmail.com>
2019-03-02 10:27:38 +0800

    ignore build

---
+++ build/precache-manifest.408e4eb85b4d2ab5e667d020c66ea2b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12801eca17177a93c36f",
+    "url": "/static/css/main.d84b0853.chunk.css"
+  },
+  {
+    "revision": "12801eca17177a93c36f",
+    "url": "/static/js/main.12801eca.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d403d261a2cfbee1412f",
+    "url": "/static/css/2.6d2a0ad4.chunk.css"
+  },
+  {
+    "revision": "d403d261a2cfbee1412f",
+    "url": "/static/js/2.d403d261.chunk.js"
+  },
+  {
+    "revision": "c5e457bc10dedc750451667e5c29a7ae",
+    "url": "/index.html"
+  }
+];
